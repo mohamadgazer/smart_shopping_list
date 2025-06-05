db7918519f2065661538b4c346c0ed09a0de45ec
mohamadGazer <mohamadgazer@yahoo.com>
2025-06-05 20:31:57 +0300

    - install device_preview: ^1.2.0 [done]

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
-
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:smart_shopping_list/exports.dart';
-void main() {
-  runApp(const MainApp());
-}
+
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MainApp(), // Wrap your app
+  ),
+);
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: routes,
-      home: const HomeView(),
-    );
+    return MaterialApp(routes: routes, home: const HomeView());
   }
 }
-
-  
